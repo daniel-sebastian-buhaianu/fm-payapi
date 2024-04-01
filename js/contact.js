7de830b343ee1f34e1b06e28ca9ef3cb1f44c896
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2024-04-01 13:01:53 +0300

    Refactor: eliminate duplicate javascript code

--- js/contact.js
+++ js/contact.js
@@ -1,34 +1,4 @@
 window.addEventListener('load', function() {
-    const mobileMenuContent = document.querySelector('.mobile-menu__content');
-
-    document
-        .querySelector('.mobile-menu__open-icon')
-        .addEventListener('click', function() {
-            removeClass('mobile-menu__content--hidden', mobileMenuContent);
-    });
-
-    mobileMenuContent
-        .querySelector('.mobile-menu__close-icon')
-        .addEventListener('click', function() {
-            addClass('mobile-menu__content--hidden', mobileMenuContent);
-    });
-
-    document.addEventListener('click', function(event) {
-        const mobileMenu = document.querySelector('.mobile-menu');
-        const targetElement = event.target;
-        if (targetElement != mobileMenu && !mobileMenu.contains(targetElement)) {
-            addClass('mobile-menu__content--hidden', mobileMenuContent);
-        }
-    });
-
-    document
-        .querySelectorAll('.demo-form__input[type="submit"]')
-        .forEach(input => {
-            input.addEventListener('click', function(event) {
-                event.preventDefault();
-            });
-        });
-
     document
         .querySelector('.contact-form__submit')
         .addEventListener('click', function(event) {
@@ -51,19 +21,3 @@ window.addEventListener('load', function() {
                 });
         });
 });
-
-function removeClass(_class, element) {
-    if (hasClass(_class, element)) {
-        element.className = element.className.replace(_class, '');
-    }
-}
-
-function addClass(_class, element) {
-    if (!hasClass(_class, element)) {
-        element.className = element.className + ' ' + _class;
-    }
-}
-
-function hasClass(_class, element) {
-    return element.className.includes(_class);
-}
